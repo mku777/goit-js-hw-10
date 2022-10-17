6ba38c849ec1d23f6d3601611a4889b513e27c55
mku777 <mkm.kuz777@gmail.com>
2022-10-17 12:38:29 +0300

    markupupd

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,7 @@
 export function fetchCountries(name) {
   const url = 'https://restcountries.com/v3.1/name/';
-  const fieldFilter = '?fields=name,capital,population,flags,languages';
-  
- return fetch(`${url}${name}${fieldFilter}`).then(response => {
+  const filter = '?fields=name,capital,population,flags,languages';
+  return fetch(`${url}${name}${filter}`).then(response => {
     return response.json();
-  })
+  });
 }
--- src/js/markUp.js
+++ src/js/markUp.js
@@ -1,16 +1,22 @@
-export function countryСard({
-  flags,
-  name,
-  capital,
-  population,
-  languages,
-}) {
+export function countryСard({ flags, name, capital, population, languages }) {
   return `
-          <img src="${flags.svg}" alt="${name.official}" width="40" />
-          <h2>${name.official}</h2>
-        <p><span>Capital:</span> ${capital}</p>
-        <p><span>Population:</span> ${population}</p>
-        <p><span>Languages:</span> ${Object.values(languages)}</p>
-    `;
+            <img src="${flags.svg}" alt="${name.official}" width="40" />
+            <h2>${name.official}</h2>
+          <p><span>Capital:</span> ${capital}</p>
+          <p><span>Population:</span> ${population}</p>
+          <p><span>Languages:</span> ${Object.values(languages)}</p>
+      `;
 }
 
+export function countryCardList({ flags, name }) {
+  return `
+    <li>
+      <img
+        src="${flags.svg}"
+        alt="${name.official}"
+        width="20"
+      />
+      <p>${name.official}</p>
+    </li>
+    `;
+}
