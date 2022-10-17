c00980f750ff762d766f59e8f564065a3d50ee49
mku777 <mkm.kuz777@gmail.com>
2022-10-17 11:34:44 +0300

    add packets + consts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './js/fetchCountries';
+import {countryСard} from './js/markUp';
+
 const DEBOUNCE_DELAY = 300;
+
+const inputSearchBox = document.querySelector("#search-box");
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
